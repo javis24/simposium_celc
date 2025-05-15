a97776375f870e608d3e83f6c9cdb59439396446
Javier Mtz <luisjaviermtzb@gmail.com>
2025-05-15 13:42:11 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+function updateCountdown(targetDate) {
+    const now = new Date().getTime();
+    const distance = targetDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById('days').innerText = days;
+    document.getElementById('hours').innerText = hours;
+    document.getElementById('minutes').innerText = minutes;
+    document.getElementById('seconds').innerText = seconds;
+}
+
+// Configura la fecha objetivo: Viernes 6 de junio, 7:30 PM
+const eventDate = new Date('June 6, 2025 19:30:00').getTime();
+
+setInterval(() => updateCountdown(eventDate), 1000);
